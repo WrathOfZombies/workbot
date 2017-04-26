84bef313b266e746ce7018e204324a0ee5c9128a
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-04-25 18:52:07 -0700

    Adding empty projects to solution for debugging, core business logic and services

---
+++ WorkBot.Debug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkBot.Debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Use this to test services from the command line");
+        }
+    }
+}
